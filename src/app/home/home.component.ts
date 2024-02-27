a9577437a749f32226a43c374be0c8dda1dad787
khaledmaati <khaled.maati@accenture.com>
2024-02-27 15:29:44 +0100

    test 3 to see if hash duplicates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -28,7 +28,7 @@ export class HomeComponent implements OnInit{
   //injecting GitInfoService
   gitInfo!: any;
 
- 
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private foodService: FoodService, 
